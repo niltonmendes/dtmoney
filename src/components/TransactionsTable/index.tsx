7e8270058b729bfa0059bf713c959dbee6119d06
Nilton Mendes <niltonjr21@hotmail.com>
2021-07-14 16:46:53 -0300

    feat: Add MirageJS for fake api

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,14 +1,30 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
 import { Container } from "./styles";
 
 export function TransactionsTable() {
+
+    useEffect(() => {
+        api.get('transactions')
+            .then(response => console.log(response.data))
+    }, []);
+
     return (
         <Container>
             <table>
                 <thead>
-                    <th>Título</th>
-                    <th>Valor</th>
-                    <th>Categoria</th>
-                    <th>Data</th>
+                    <tr>
+                        <th>Título</th>
+                    </tr>
+                    <tr>
+                        <th>Valor</th>
+                    </tr>
+                    <tr>
+                        <th>Categoria</th>
+                    </tr>
+                    <tr>
+                        <th>Data</th>
+                    </tr>
                 </thead>
                 <tbody>
                     <tr>
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs';
 import { App } from './App';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Desenvolvimento Web',
+          amount: 12000,
+          type: 'deposit',
+          category: 'Desenvolvimento',
+          createdAt: new Date(),
+        },
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
