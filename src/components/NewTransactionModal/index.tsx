d7787b96e015ba78f27ca9861c4b4fed12f8f3d2
Nilton Mendes <niltonjr21@hotmail.com>
2021-07-14 18:59:17 -0300

    chore: Modal input data finalized and fake api update

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,13 @@
+import { FormEvent, useState } from 'react';
 import Modal from 'react-modal';
-
+import { api } from '../../services/api';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
 import closeImg from '../../assets/close.svg';
-import { Container } from './styles';
+
+import { Container, TransactionTypeContainer, RadioBox } from './styles';
+
+
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -9,6 +15,25 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose}: NewTransactionModalProps) {
+    const [title, setTitle] = useState('');
+    const [amount, setAmount] = useState(0);
+    const [category, setCategory] = useState('');
+    const [type, setType] = useState('deposit');
+
+    function handleCreateNewTransaction(event: FormEvent) {
+        event.preventDefault();
+
+        const data = {
+            title,
+            amount,
+            category,
+            type,
+        }
+
+        api.post('/transactions', data);
+
+    }
+    
     return (
         <Modal
             isOpen={isOpen}
@@ -24,18 +49,48 @@ export function NewTransactionModal({ isOpen, onRequestClose}: NewTransactionMod
                 <img src={closeImg} alt="Fechar" />
             </button>
 
-            <Container>
+            <Container 
+                onSubmit={handleCreateNewTransaction}
+            >
                 <h2>Cadastrar transação</h2>
 
                 <input
                     placeholder="Título"
+                    value={title}
+                    onChange={event => setTitle(event.target.value)}
                 />
                 <input
                     type="number"
                     placeholder="Valor"
+                    value={amount}
+                    onChange={event => setAmount(Number(event.target.value))}
                 />
+
+                <TransactionTypeContainer>
+                    <RadioBox
+                        type="button"
+                        onClick={() => {setType('deposit')}}
+                        isActive={type === 'deposit'}
+                        activeColor="green"
+                    >
+                        <img src={incomeImg} alt="Entrada" />
+                        <span>Entrada</span>
+                    </RadioBox>
+                    <RadioBox
+                        type="button"
+                        onClick={() => {setType('withdraw')}}
+                        isActive={type === 'withdraw'}
+                        activeColor="red"
+                    >
+                        <img src={outcomeImg} alt="Saída" />
+                        <span>Saída</span>
+                    </RadioBox>
+                </TransactionTypeContainer>
+
                 <input
                     placeholder="Categoria"
+                    value={category}
+                    onChange={event => setCategory(event.target.value)}
                 />
                 
                 <button type="submit">
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
 
 createServer({
+
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelancer de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:00:30')
+        },
+        {
+          id: 2,
+          title: 'Cinema',
+          type: 'withdraw',
+          category: 'Lazer',
+          amount: 100,
+          createdAt: new Date('2021-05-14 21:00:00')
+        }
+      ],
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Desenvolvimento Web',
-          amount: 12000,
-          type: 'deposit',
-          category: 'Desenvolvimento',
-          createdAt: new Date(),
-        },
-      ]
+      return this.schema.all('transaction');
+    })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+      return schema.create('transaction', data);
     })
   }
 })
