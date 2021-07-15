4da2dfcc092cb1b5a8e3594442faf111249ad61d
Nilton Mendes <niltonjr21@hotmail.com>
2021-07-15 16:35:18 -0300

    chore: Transform TransactionsContext in hook useTransactions

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,11 +1,10 @@
-import { useContext } from "react";
-import { TransactionsContext } from "../../TransactionsContext";
+import { useTransactions } from "../../hooks/useTransactions";
 
 import { Container } from "./styles";
 
 
 export function TransactionsTable() {
-    const { transactions } = useContext(TransactionsContext);
+    const { transactions } = useTransactions();
     
     return (
         <Container>
@@ -31,7 +30,7 @@ export function TransactionsTable() {
                             <td>{transaction.category}</td>
                             <td>
                                 {new Intl.DateTimeFormat('pt-BR')
-                                    .format(new Date(transaction.amount))
+                                    .format(new Date(transaction.createdAt))
                                 }
                             </td>
                         </tr>
